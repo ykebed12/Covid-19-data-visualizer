1b867614053d7eaf4d48eb2d71ce3a80e20f25c6
ykebed12 <ykebede2@umbc.edu>
2021-11-22 18:04:55 -0500

    Fixed styling and legend change on theme color switch

--- static/Functions.js
+++ static/Functions.js
@@ -1,7 +1,7 @@
 // Function that takes in county cases and 
 // returns the hex color (e.g #FF3F43) associated with it
 // with red and green extremes
-function getColor(cases) {
+function getColorRG(cases) {
 
 	return  (cases > 1000000) 	? 	"#ff0000":
 			(cases > 100000) 	?	"#ff8000":
@@ -20,3 +20,23 @@ function getColorMono(cases) {
 									"#eff3ff"
 
 }
+
+function getColor(cases) {
+
+	return document.getElementById("monocolor").checked ? getColorMono(cases) : getColorRG(cases)
+}
+
+function makeLegend (map) {
+
+	var div = L.DomUtil.create('div', 'info legend'),
+		grades = [0, 1000, 10000, 100000, 1000000]
+
+	// loop through our intervals and generate a label with a colored square for each interval
+	for (var i = 0; i < grades.length; i++) {
+		div.innerHTML += '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+			grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	}
+
+	return div;
+};
+
